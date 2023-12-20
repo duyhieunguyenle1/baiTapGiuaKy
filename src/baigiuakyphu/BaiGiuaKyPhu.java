d3e8e86d49145cbdd3753392d490de07aa31b2dd
hieunld1011 <hieunld@vmogroup.com>
2023-12-20 10:53:20 +0700

    first commit

---
+++ src/baigiuakyphu/BaiGiuaKyPhu.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package baigiuakyphu;
+
+/**
+ *
+ * @author Admin
+ */
+public class BaiGiuaKyPhu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new DanhSachSinhVien().setVisible(true);
+    }
+}
